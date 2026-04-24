dbd682384303101087f326b18b97cf18c9b16c62
MysteryCoder <srikar0811@gmail.com>
2026-04-24 16:46:29 +0530

    use local emulators only if set in env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
-import { getStorage, connectStorageEmulator } from 'firebase/storage';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
@@ -17,14 +17,17 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 // Connect to local emulators in development if true
-if (process.env.NODE_ENV === 'development') {
-  try {
-    connectFirestoreEmulator(db, '127.0.0.1', 8080);
-    connectStorageEmulator(storage, '127.0.0.1', 9199);
-    console.log('Connected to Firebase Emulators');
-  } catch (e) {
-    // Ignore if already connected
-  }
+if (
+	process.env.NODE_ENV === "development" &&
+	process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATORS
+) {
+	try {
+		connectFirestoreEmulator(db, "127.0.0.1", 8080);
+		connectStorageEmulator(storage, "127.0.0.1", 9199);
+		console.log("Connected to Firebase Emulators");
+	} catch (e) {
+		// Ignore if already connected
+	}
 }
 
 export default app;
